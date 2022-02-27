bb72b9dfb97bd2d33e3a91660c68a82ac4b9c333
Sebastian Siemssen <fubhy@fubhy.com>
2022-02-27 10:06:41 +0100

    feat: add script to check event handlers

---
+++ scripts/commands/check-handlers.ts
@@ -0,0 +1,50 @@
+import fs from 'fs-extra';
+import subgraphDefinition from '../../manifest';
+
+export const command = 'check-handlers';
+export const description = 'Verify that every event has a matching handler';
+
+export async function handler() {
+  for (const source of subgraphDefinition.dataSources) {
+    const sourceFile = source.mapping.file;
+    const sourceFileContent = await fs.readFile(sourceFile, 'utf8');
+
+    // Find all defined event handlers based on this regex.
+    const handlerMatches = sourceFileContent.matchAll(/export function handle(?<handler>[A-Z]\w*)\(/gi);
+    const definedHandlers = Array.from(handlerMatches)
+      .map((item) => item.groups?.handler)
+      .filter(Boolean);
+
+    const expectedHandlers = source.mapping.eventHandlers;
+
+    // We expect a dedicated event handler for each abi event type.
+    const missingHandlers = expectedHandlers
+      .filter((item) => !definedHandlers.some((inner) => `handle${inner}` === item.handler))
+      .map((handler) => handler.handler.replace(/^handle/, ''));
+
+    // Check that there are no unused handlers.
+    const unusedHandlers = definedHandlers.filter(
+      (item) => !expectedHandlers.some((inner) => inner.handler === `handle${item}`),
+    );
+
+    if (!unusedHandlers.length && !missingHandlers.length) {
+      console.log('There are no missing or unused event handlers. All good!');
+      return;
+    }
+
+    console.log('========================================');
+    console.log(`DATA SOURCE ${source.name}:\n`);
+
+    if (unusedHandlers.length) {
+      console.log(`UNUSED: ${unusedHandlers.join(', ')}`);
+    }
+
+    if (missingHandlers.length) {
+      console.log(`MISSING: ${missingHandlers.join(', ')}`);
+    }
+
+    console.log('========================================\n');
+  }
+
+  process.exit(1);
+}
--- scripts/commands/generate-abis.ts
+++ scripts/commands/generate-abis.ts
@@ -1,42 +1,41 @@
 import fs from 'fs-extra';
 import path from 'path';
-import glob from 'glob';
 import { utils } from 'ethers';
 import { JsonFragment } from '@ethersproject/abi';
 import { packageRoot } from '../constants';
 
 export interface ProcessedArtifact {
-  absolutePath: string;
-  relativePath: string;
+  sourcePath: string;
+  destinationPath: string;
   contractName: string;
   contractInterface: utils.Interface;
   rawJson: JsonFragment[];
   cleanedJson: JsonFragment[];
 }
 
-export const command = 'clean-abis';
+export const command = 'generate-abis';
 export const description = 'Extracts & clean abis from the deployment output';
 
-async function getAbis(source: string) {
-  const files = glob.sync('**/*.json', {
-    cwd: path.join(packageRoot, source),
-    absolute: true,
-  });
+async function getAbis() {
+  const artifacts = {
+    Alchemist: path.resolve(packageRoot, 'artifacts/localhost/AlchemistV2_alETH.json'),
+    Transmuter: path.resolve(packageRoot, 'artifacts/localhost/TransmuterV2_ETH.json'),
+    TransmuterBuffer: path.resolve(packageRoot, 'artifacts/localhost/TransmuterBuffer_alETH.json'),
+  } as const;
 
   const contents = await Promise.all(
-    files.map<Promise<ProcessedArtifact>>(async (absolutePath) => {
-      const relativePath = path.relative(packageRoot, absolutePath);
-      const rawJson = (await fs.readJson(absolutePath)) as JsonFragment[];
+    Object.entries(artifacts).map<Promise<ProcessedArtifact>>(async ([contractName, sourcePath]) => {
+      const rawJson = (await fs.readJson(sourcePath)).abi as JsonFragment[];
+      const destinationPath = path.resolve(packageRoot, 'abis', `${contractName}.json`);
       const contractInterface = new utils.Interface(rawJson);
-      const contractName = path.basename(relativePath, '.json');
 
       // TODO: Remove this once graph-node support solc v0.8+ custom error types.
       // https://github.com/graphprotocol/graph-node/issues/2577
       const cleanedJson = rawJson.filter((item) => item.type !== 'error');
 
       return {
-        absolutePath,
-        relativePath,
+        sourcePath,
+        destinationPath,
         contractName,
         contractInterface,
         rawJson,
@@ -49,8 +48,8 @@ async function getAbis(source: string) {
 }
 
 export async function handler() {
-  const extractedAbis = await getAbis('abis');
+  const extractedAbis = await getAbis();
   for (const currentAbi of extractedAbis) {
-    await fs.writeJson(currentAbi.absolutePath, currentAbi.cleanedJson);
+    await fs.outputJson(currentAbi.destinationPath, currentAbi.cleanedJson);
   }
 }
